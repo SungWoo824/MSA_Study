9350cb8aca86eed7ad42321c4a0c642b0c80ef6a
SungWoo824 <scody1219@gmail.com>
2021-06-11 19:58:47 +0900

    add : service implement

---
+++ multiplication/src/main/java/com/example/multiplication/service/RandomGeneratorServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.multiplication.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RandomGeneratorServiceImpl implements RandomGeneratorService{
+    @Override
+    public int generateRandomFactor() {
+        return 0;
+    }
+}
