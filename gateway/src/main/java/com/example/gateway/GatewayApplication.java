1f0a372b04860ab83b37c8d355d8363bff22edae
SungWoo824 <scody1219@gmail.com>
2021-09-01 21:31:24 +0900

    fix : gateway

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -3,7 +3,6 @@ package com.example.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class GatewayApplication {
 
